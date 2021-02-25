66300623a86272e0b6ec171f3061360b596ab46c
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-02-25 13:40:34 +0100

    Zadanie_lesson3

---
+++ src/first_week/Zadania/Lekcja_3.java
@@ -0,0 +1,19 @@
+package first_week.Zadania;
+
+public class Lekcja_3 {
+    //Zadanie_1
+    public static void main(String[] args) {
+        // Poznane zmienne
+        System.out.println("Dzisiaj poznalismy: int , double , char , string");
+        //Zadanie_2
+        int myIntA = 4;
+        int myIntB = 6;
+        int sum = myIntA * myIntA + myIntB * myIntB;
+        System.out.println("Wynik: " + sum );
+        //Zadanie_4
+        String myStringA = "Adrian";
+        String myStringB = "Swiatelski";
+
+        System.out.println(myStringA + "\t" + myStringB);
+    }
+}
