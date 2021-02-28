cb4bbde6996c0ee68ca7a49cde7f66cb2f674bef
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-02-28 20:19:29 +0100

    Zad_1

---
+++ src/first_week/loops/Zad_1.java
@@ -0,0 +1,18 @@
+package first_week.Zadania;
+import java.util.Scanner;
+public class Zad_1 {
+public static void main(String[] args) {
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int a = skaner.nextInt();
+        if (a > 0) {
+        System.out.println("Liczba jest większa od zera");
+        }
+        if (a < 0) {
+        System.out.println("Liczba jest mniejsza od zera");
+        }
+        if (a == 0) {
+        System.out.println("Liczba jest równa zeru");
+        }
+        }
+        }
