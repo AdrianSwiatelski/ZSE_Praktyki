58b6d050d2e5a3015c48bbfcbe61253f5444aa53
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-03-01 20:55:53 +0100

    Zad_2

---
+++ src/first_week/loops/Zad_2_two.java
@@ -0,0 +1,28 @@
+package first_week.loops;
+import java.util.Scanner;
+public class Zad_2_two {
+    public static void main(String[] args) {
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę: ");
+        int a = skaner.nextInt();
+        System.out.println("Podaj drugą liczbę: ");
+        int b = skaner.nextInt();
+        System.out.println("Podaj trzecią liczbę: ");
+        int c = skaner.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println("Największa liczba to: " + a);
+            }
+        }
+        if (a < b) {
+            if (c < b) {
+                System.out.println("Największa liczba to: " + b);
+            }
+        }
+        if (a > b) {
+            if (a < c) {
+                System.out.println("Największa liczba to: " + c);
+            }
+        }
+    }
+}
