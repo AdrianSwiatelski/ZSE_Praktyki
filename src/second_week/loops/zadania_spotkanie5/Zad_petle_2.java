94bc524a939a569fd81e85631e619562b68d8f57
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-03-02 18:53:11 +0100

    Zad2_Pętle

---
+++ src/second_week/loops/zadania_spotkanie5/Zad_petle_2.java
@@ -0,0 +1,10 @@
+package second_week.loops.zadania_spotkanie5;
+
+public class Zad_petle_2 {
+    public static void main(String[] args) {
+        System.out.println("Kolejność malejąca co 3 od 30: ");
+        for (int number = 30; number >= 0; number-=3) {
+            System.out.println(number + "\t");
+        }
+    }
+}
