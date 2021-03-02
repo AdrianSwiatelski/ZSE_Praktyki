4cd29c49ccc3361585c6fe6257160edbe1c9a7e5
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-03-02 18:52:47 +0100

    Zad1_Pętle

---
+++ src/second_week/loops/zadania_spotkanie5/Zad_petle_1.java
@@ -0,0 +1,10 @@
+package second_week.loops.zadania_spotkanie5;
+
+public class Zad_petle_1 {
+    public static void main(String[] args) {
+        System.out.println("Kolejność rosnąca co 2: ");
+        for (int number = 0; number <= 20; number += 2) {
+            System.out.println(number + "\t");
+        }
+    }
+}
---
+++ src/second_week/tables/Zad_tablice1.java
@@ -0,0 +1,7 @@
+package second_week.tables;
+
+public class Zad_tablice1 {
+    public static void main(String[] args) {
+        
+    }
+}
