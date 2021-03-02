891a6a1fd10184f2bad560153390102cda094672
AdrianSwiatelski <79515159+AdrianSwiatelski@users.noreply.github.com>
2021-03-02 19:18:25 +0100

    Zad1_Tablice

--- src/second_week/tables/Zad_tablice1.java
+++ src/second_week/tables/Zad_tablice1.java
@@ -1,7 +1,11 @@
 package second_week.tables;
 
+import java.util.Arrays;
+
 public class Zad_tablice1 {
     public static void main(String[] args) {
-        
+        char [] charArray = {'f' , 'c' , '$' , 'm' , '#' , 'p' , '*' , 'w' , 'u' , '@'};
+        Arrays.sort(charArray);
+        System.out.println("\n Sortowanie rosnące: " + Arrays.toString(charArray));
     }
 }
